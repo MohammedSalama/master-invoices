812942d513423064bd9abc0539206275ebe96e58
MohammedSalama <mohamed96salama@gmail.com>
2023-03-25 02:01:39 +0200

    Crud-Invoices

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,11 +29,9 @@ Route::get('/home', function () {
     return view('home');
 }) ->middleware(['auth', 'verified'])->name('home');
 
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
+
 
 require __DIR__.'/auth.php';
+
+Route::resource('invoices',InvoicesController::class);
 Route::get('/{page}', [AdminController::class,'index']);
